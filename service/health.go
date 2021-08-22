65c9f1cd14814c42d9b0089f94dd973708c881cb
Marcelo Sales <marcelo.d.sales@gmail.com>
2021-08-22 18:21:14 +0200

    import project

---
+++ service/health.go
@@ -0,0 +1,22 @@
+package service
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+// HealthCheck godoc
+// @Summary Show the status of server.
+// @Description get the status of server.
+// @Tags root
+// @Accept */*
+// @Produce json
+// @Success 200 {object} map[string]interface{}
+// @Router / [get]
+func HealthCheck(c *gin.Context) {
+	res := map[string]interface{}{
+		"data": "Server is up and running",
+	}
+
+	c.JSON(http.StatusOK, res)
+}
